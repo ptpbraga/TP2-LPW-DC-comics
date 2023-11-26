c949e409bbec2637cf9ebb60f1dda225f60abb4f
mariapaulamaia <133915720+mariapaulamaia@users.noreply.github.com>
2023-11-26 23:02:25 +0000

    flipcard e ajax, falta batfamily e viloes

--- javascript/personagens.js
+++ javascript/personagens.js
@@ -4,3 +4,23 @@ var swiper = new Swiper('.Slider-container', {
     centerdSlides: true,
     loop: true,
   });
+
+
+
+$.ajax({
+  url: 'personagens.json',
+  dataType: 'json',
+  success: resposta => {
+    let results = resposta.results;
+    $('#aquaman-h1').html(`${results[0].name}`);
+    $('#arrow-h1').html(`${results[1].name}`);
+    $('#batman-h1').html(`${results[2].name}`);
+    $('#cacador-h1').html(`${results[3].name}`);
+    $('#flash-h1').html(`${results[4].name}`);
+    $('#maravilha-h1').html(`${results[5].name}`);
+    $('#tornado-h1').html(`${results[7].name}`);
+    $('#superman-h1').html(`${results[6].name}`);
+    $('#zatana-h1').html(`${results[8].name}`);
+  }
+});
+
