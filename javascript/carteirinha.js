4ea172b3f28f398484c5d44a4ce809022387463d
Pedro Tolentino Pires Braga <132150870+ptpbraga@users.noreply.github.com>
2023-11-27 17:59:54 -0300

    Update carteirinha.js
    Adicionando o input das habilidades

--- javascript/carteirinha.js
+++ javascript/carteirinha.js
@@ -3,15 +3,20 @@ $(document).ready(function() {
       var salvanome = $("#nome").val();
       var salvadata = $("#data_aniversario").val();
       var salvacor = $("#cor").val();
+      var salvaHabilidades = $("#habil").val();
+      
 
       localStorage.setItem("nomeperfil", salvanome);
       localStorage.setItem("datanas", salvadata);
       localStorage.setItem("corequipe", salvacor);
+      localStorage.setItem("habilidades", salvaHabilidades);
+
     });
 
     $("#carregar").click(function() {
       $("#name").html(localStorage.getItem("nomeperfil"));
       $("#nasceu").html(localStorage.getItem("datanas"));
+      $("#hab").html(localStorage.getItem("habilidades"));
 
       var colore = localStorage.getItem("corequipe");
       $(".carteirinha").css("background-color", colore);
