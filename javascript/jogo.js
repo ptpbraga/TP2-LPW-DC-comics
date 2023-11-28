201049dd606c5adc766b0d0690b36149f195e4c3
mariapaulamaia <133915720+mariapaulamaia@users.noreply.github.com>
2023-11-28 12:13:45 -0300

    Update jogo.js

--- javascript/jogo.js
+++ javascript/jogo.js
@@ -1,5 +1,5 @@
   $("#claro").click( function() {
-    $("#p-jogo").html("Muito bem pequeno detetive! Preciso que você ache quem contruiu esse site. Há eles revelaram minha identidade secreta!");
+    $("#p-jogo").html("Muito bem pequeno detetive! Preciso que você ache quem construiu esse site. Eles revelaram minha identidade secreta!");
     $("#claro").remove();
     $("#nao").remove();
     $("#balao").append('<a href="index.html"><button class="volte"> Voltar a página inicial</button></a>');
